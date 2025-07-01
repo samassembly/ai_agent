b44eb0f7424f3af47b6a13392e5335aaf3946605
SamPCS <svogel@pcsknox.com>
2025-07-01 16:16:19 -0400

    Added Verbose flag as optional parameter

--- main.py
+++ main.py
@@ -13,15 +13,20 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
-    
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
 
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+
+    #Check for verbose
+    if '--verbose' in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
 
 if __name__ == "__main__":
     main()
