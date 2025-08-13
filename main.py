f3f3db93151c79f440a7d60178fac6793fc3166f
SamPCS <svogel@pcsknox.com>
2025-08-13 16:01:22 -0400

    Looped generate content calls, successfully fixed bug in calculator app

--- main.py
+++ main.py
@@ -16,11 +16,31 @@ def main():
         sys.exit(1)
 
     load_dotenv()
+    verbose = "--verbose" in sys.argv
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     user_prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
 
+    #Calling generate content model loop
+    iterator = 0
+    max_iteration = 20
+
+    while True:
+        iterator += 1
+        if iterator > max_iteration:
+            print(f"Terminating loop, too many iterations")
+            sys.exit(1)
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                sys.exit(0)
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+
+def generate_content(client, messages, verbose):
     system_prompt = """
                     You are a helpful AI coding agent.
 
@@ -33,43 +53,36 @@ def main():
 
                     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
-
-    model_name = 'gemini-2.0-flash-001'
     available_functions = types.Tool(
         function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file]
     )
-
-    #Calling generate content model loop
-    
-    for i in range(20):
-        try: 
-            response = client.models.generate_content(model=model_name, contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),)
-        except google.genai.errors.ClientError as e:
-            print(f"An error has occurred: {e}")
-
-        if response.text:
-            print(response.text)
-            break #response found, break out of loop
-        prompt_tokens = response.usage_metadata.prompt_token_count
-        response_tokens = response.usage_metadata.candidates_token_count
-        function_call_part = response.function_calls[0]
-        response_candidates = response.candidates
-        #Loop candidates and append to messages list
-        for candidate in response_candidates:
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),)
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+    if response.candidates:
+        for candidate in response.candidates:
             messages.append(candidate.content)
-        #convert function_responses into a message with role of user
+    if not response.function_calls:
+        return response.text
+        
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
 
-    
-    #Check for verbose
-    if '--verbose' in sys.argv:
-        print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {prompt_tokens}")
-        print(f"Response tokens: {response_tokens}")
-    if function_call_part:
-        function_call_result = call_function(function_call_part)
-        print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(types.Content(role="user", parts=function_responses))
 
 if __name__ == "__main__":
     main()
